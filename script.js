07643541d7d902a0946482f53814e8dc418440d6
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-08-23 19:23:58 -0500

    Connect operate function to buttons

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ function operate(op, a, b) {
     return add(a, b);
   } else if (op === '-') {
     return subtract(a, b);
-  } else if (op === '*') {
+  } else if (op === 'x') {
     return multiply(a, b);
-  } else if (op === '/') {
+  } else if (op === '÷') {
     return divide(a, b);
   }
 }
@@ -32,10 +32,34 @@ const display = document.querySelector('.display');
 const displayText = document.querySelector('.display-text');
 const numberButtons = document.querySelectorAll('.number-btn');
 const operatorButtons = document.querySelectorAll('.operator-btn');
+const buttons = document.querySelectorAll('button');
 
 for (let i = 0; i < numberButtons.length; i++) {
   numberButtons[i].addEventListener('click', function (e) {
     let displayValue = e.target.textContent;
     displayText.textContent += `${displayValue}`;
+  });
+}
+
+let a = '';
+let b = '';
+let op = '';
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function (e) {
+    if (e.target.classList.contains('number-btn') && !op) {
+      a += e.target.textContent;
+      console.log(`a = ${a}`);
+    } else if (!op && e.target.classList.contains('operator-btn')) {
+      op = e.target.textContent;
+      console.log(`op = ${op}`);
+    } else if (a && op && e.target.classList.contains('number-btn')) {
+      b += e.target.textContent;
+      console.log(`b = ${b}`);
+    } else if (op && e.target.classList.contains('operator-btn')) {
+      a = Number(a);
+      b = Number(b);
+      console.log(operate(op, a, b));
+    }
   });
 }
